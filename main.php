c370adbe0f99024b877a5c005c733180c45157f5
bikash banjade <92980908+bikas15@users.noreply.github.com>
2022-08-11 23:05:10 +0545

    Update main.php

--- main.php
+++ main.php
@@ -1,11 +1,23 @@
 <?php
-$connection=mysqli_connect("root","localhost","form");
+$connection=mysqli_connect("sql105.unaux.com","unaux_31384517","bikashens","unaux_31384517_form");
 if(!$connection)
 {
     echo "server coudn't found";
 }
 else{
-    "connected"
+    echo "connected";
 }
+$name=$_POST['username'];
+$email=$_POST['useremail'];
+$msg=$_POST['msg'];
+$query =   "INSERT INTO bikas(name,email,msg) VALUES
+('{$name}','{$email}','{$msg}')";
+$result = mysqli_query($connection,$query);
+if($result){
+  echo "Record Saved Successfully";
+}else{
+  die("Database Query Failed" . mysqli_error($connection));
+}
+
+
 ?>
-hello
